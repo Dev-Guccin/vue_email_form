2d61acb78d768f525687c8baf5267359be945156
wodhks08 <ghks2047@naver.com>
2021-11-22 10:18:50 +0900

    fix: improve upload script

--- upload.sh
+++ upload.sh
@@ -1,3 +1,5 @@
 npm run build
 sudo rm /var/www/html/dist -r
-cp /home/ghks/email/dist /var/www/html/ -r
+sed -i -e "s/\"\/css/\".\/css/g" dist/index.html
+sed -i -e "s/\"\/js/\".\/js/g" dist/index.html
+cp /home/ghks/vue_email_form/dist /var/www/html/ -r
